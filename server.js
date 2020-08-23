c05b1575cf7ec479a0e04b344e3281c8390f4636
Usamath PC <usamathpallikkachalil@gmail.com>
2020-08-23 11:59:22 +0530

    final edits

--- server.js
+++ server.js
@@ -4,26 +4,45 @@ const axios = require("axios");
 
 let port = process.env.PORT || 5000; //process.env.PORT to get whatever port available in the env variable PORT
 
+const getknrJSON = (response) => {
+  let knrFeatures = response.data.features.filter((obj) => {
+    return obj.properties.district === "Kannur";
+  });
+  let filteredJSON = { type: "FeatureCollection", features: knrFeatures };
+  return filteredJSON;
+};
+
 //route1 access from client
 app.get("/", (req, res) => {
   res.send(
-    "Welcome to Covid Kannur API.\n\n Use '/API/containmentLSGs for Containment Zones JSON data'"
+    "Welcome to Covid Kannur API. \n\n Use '/API/hotspots' route for Containment Zones GeoJSON data. \n\n Use '/API/lsglist' for list of containment zones and containment wards within each zone."
   );
 });
 
-//route 2
-app.get("/API/containmentLSGs", async (req, res) => {
+//route 2 lsg data
+app.get("/API/hotspots", async (req, res) => {
+  let response = await axios.get(
+    "https://hotspot-api.ngh.staging.n1sh.com/hotspots/latest.json"
+  );
+  let knrJSON = getknrJSON(response);
+  res.send(knrJSON);
+});
+
+//route 3
+app.get("/API/lsglist", async (req, res) => {
   let response = await axios.get(
     "https://hotspot-api.ngh.staging.n1sh.com/hotspots/latest.json"
   );
 
-  let knrFeatures = response.data.features.filter((obj) => {
-    return obj.properties.district === "Kannur";
+  let temp = getknrJSON(response).features.map((obj) => {
+    return {
+      id: obj.properties.id,
+      label: obj.properties.label,
+      czones: obj.properties.notes,
+    };
   });
 
-  let filteredJSON = { type: "FeatureCollection", features: knrFeatures };
-
-  res.send(filteredJSON);
+  res.send(temp);
 });
 
 //when server run will listen on this port.
