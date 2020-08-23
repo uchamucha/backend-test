d83c112714e8df68f8ced19aa78752ed2d26f08e
Usamath PC <usamathpallikkachalil@gmail.com>
2020-08-23 10:36:11 +0530

    minor edits

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const app = express();
-
-const data = require("./data.json");
 const axios = require("axios");
 
 let port = process.env.PORT || 5000; //process.env.PORT to get whatever port available in the env variable PORT
 
 //route1 access from client
 app.get("/", (req, res) => {
-  res.send("Hello From Expresssss!!!");
+  res.send(
+    "Welcome to Covid Kannur API.\n\n Use '/API/containmentLSGs for Containment Zones JSON data'"
+  );
 });
 
 //route 2
-app.get("/API/data", (req, res) => {
-  res.send(data);
-});
-
-//route 3
-app.get("/API/lsg", async (req, res) => {
+app.get("/API/containmentLSGs", async (req, res) => {
   let response = await axios.get(
     "https://hotspot-api.ngh.staging.n1sh.com/hotspots/latest.json"
   );
