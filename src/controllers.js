0998c4b143d509531f9cc0a8566a9f5b9ad6f31c
Usamath PC <usamathpallikkachalil@gmail.com>
2020-12-05 00:03:51 +0530

    reorganized file structure

---
+++ src/controllers.js
@@ -0,0 +1,33 @@
+exports.getDistJSON = async (district) => {
+  let response = await axios.get(
+    "https://hotspot-api.ngh.staging.n1sh.com/hotspots/latest.json"
+  );
+  let distFeatures = response.data.features.filter((obj) => {
+    return obj.properties.district === district;
+  });
+
+  console.log(distFeatures[31].geometry.coordinates);
+
+  //adding centroid to each feature
+  distFeatures.forEach((feature) => {
+    feature.properties.centroid = {};
+
+    if (feature.geometry.coordinates.length === 1) {
+      feature.properties.centroid.long = polylabel(
+        feature.geometry.coordinates,
+        1
+      )[0];
+
+      feature.properties.centroid.lat = polylabel(
+        feature.geometry.coordinates,
+        1
+      )[1];
+    } else if (feature.geometry.coordinates.length === 2) {
+      feature.properties.centroid = feature.geometry.coordinates;
+    }
+  });
+
+  let distJSON = { type: "FeatureCollection", features: distFeatures };
+
+  return distJSON;
+};
